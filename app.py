98991147cec1a10ad8be150af7332cb5f77f9dfb
krevindev <alimpuangonkylerevin@gmail.com>
2025-08-13 04:38:54 +0800

    renamed the tokens

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import docx
 from io import BytesIO
 
 # --- CONFIG ---
-VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "verify_token")
-PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "")
+VERIFY_TOKEN = os.getenv("FB_VERIFY_TOKEN", "verify_token")
+PAGE_ACCESS_TOKEN = os.getenv("FB_PAGE_ACCESS_TOKEN", "")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
 MODEL = "mistralai/mixtral-8x7b-instruct"
 
