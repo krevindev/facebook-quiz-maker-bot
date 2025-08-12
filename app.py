bfd694150b70671def732249d851da13c7f526e7
krevindev <alimpuangonkylerevin@gmail.com>
2025-08-13 04:19:08 +0800

    renamed the access token

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import docx
 
 # ----------------- Config -----------------
 VERIFY_TOKEN = os.getenv("FB_VERIFY_TOKEN", "quiz_000")
-PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "")
+PAGE_ACCESS_TOKEN = os.getenv("FB_PAGE_ACCESS_TOKEN", "")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
 OPENROUTER_MODEL = "mistralai/mixtral-8x7b-instruct"
 
